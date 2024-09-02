113cf631fb8981bd40b03a1ab6d084481c3025c4
David Osorio <daalosx@gmail.com>
2024-09-02 12:43:47 -0500

    eliminando la carpeta lib

--- global.d.ts
+++
@@ -1,8 +0,0 @@
-type de = typeof import('./messages/de.json');
-type en = typeof import('./messages/en.json');
-type es = typeof import('./messages/es.json');
-type fr = typeof import('./messages/fr.json');
-type po = typeof import('./messages/po.json');
-type ru = typeof import('./messages/ru.json');
-
-declare interface IntlMessages extends de, en, es, fr, po, ru {}
