9847cfe1ac9cb41ad1515b62d28d63206df4d072
David Osorio <daalosx@gmail.com>
2024-09-21 23:16:39 -0500

    areglando el deployt

---
+++ app/[locale]/auth/login/layout.tsx
@@ -0,0 +1,7 @@
+export default function Login ( {children} ){
+    return (
+        <section className="z-10 h-screen w-screen absolute top-0 left-0 flex justify-end max-lg:bg-lessBlack/70 lg:bg-auth ">
+            {children}
+        </section>
+    )
+}
